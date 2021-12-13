940c99be33af85df6a851d2f06916fcb49b9c33f
klechko <tin.klecina@gmail.com>
2021-12-13 19:37:15 +0100

    Made the nav responsive

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const navTranslate = () => {
+  const burger = document.querySelector('.burger');
+  const menuIcon = document.querySelector('.fas')
+  const navigation = document.querySelector('#navigation');
+  let check = false;
+
+  burger.addEventListener('click', () => {
+    navigation.classList.toggle('nav-active');
+    check = !check;
+    // console.log(check);
+    if(check) {
+      menuIcon.classList.remove('fa-bars');
+      menuIcon.classList.add('fa-times');
+    }
+    else {
+      menuIcon.classList.remove('fa-times');
+      menuIcon.classList.add('fa-bars');
+    }
+  });
+}
+
+navTranslate();
